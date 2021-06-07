e37d76e3ad58127599218246c0ecfbff7f5406c4
WAI_HAN_KO <waihanko.dev@gmail.com>
2021-06-07 09:37:17 +0630

    - Theme Color

--- lib/resources/colors.dart
+++ lib/resources/colors.dart
@@ -9,6 +9,7 @@ const pinkColor = {
   NORMAL_TEXT_COLOR: Color(0xff000000),
   TOTAL_DAY_COLOR: Color(0xffff495a),
   PROGRESS_COLOR: Color(0xffff495a),
+  PROGRESS_BG_COLOR: Color(0xfffcb8be),
   STATUS_BAR_COLOR: Color(0xfffd7f75),
   CARD_COLOR: Color(0xfffff6f5),
 };
@@ -20,6 +21,7 @@ const blueColor = {
   NORMAL_TEXT_COLOR: Color(0xff000000),
   TOTAL_DAY_COLOR: Color(0xff0075D4),
   PROGRESS_COLOR: Color(0xff0075D4),
+  PROGRESS_BG_COLOR: Color(0xffa6cfee),
   STATUS_BAR_COLOR: Color(0xff0284f8),
   CARD_COLOR: Color(0xfff5fbff),
 };
@@ -32,6 +34,7 @@ const greenColor = {
   NORMAL_TEXT_COLOR: Color(0xff000000),
   TOTAL_DAY_COLOR: Color(0xff728d2c),
   PROGRESS_COLOR: Color(0xff728d2c),
+  PROGRESS_BG_COLOR: Color(0xffe8ffb2),
   CARD_COLOR: Color(0xfff3fcf5),
 };
 
@@ -42,6 +45,7 @@ const purpleColor = {
   NORMAL_TEXT_COLOR: Color(0xff000000),
   TOTAL_DAY_COLOR: Color(0xff7b5fde),
   PROGRESS_COLOR: Color(0xff7b5fde),
+  PROGRESS_BG_COLOR: Color(0xffd0c5fd),
   STATUS_BAR_COLOR: Color(0xff9980E9),
   CARD_COLOR: Color(0xfff7f5ff),
 };
